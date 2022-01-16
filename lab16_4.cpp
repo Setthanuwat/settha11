db6a7c10cea87b7431f7ca9db408b3f591fe1f86
Setthanuwat <aftnick2@gmail.com>
2022-01-17 01:10:59 +0700

    "update"

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,53 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	int R1 , R2 , R3 , R4 , temp ;
+	R1 = rand()%2 ;
+	R2 = rand()%2 ;
+	R3 = rand()%2 ;
+	R4 = rand()%2 ;
+
+	if(R1 == 0)
+	{
+		temp = a ;
+		a = b ;
+		b = temp;
+	}
+	if(R1 == 1)
+	{
+		;
+	}
+	if(R2 == 0)
+	{
+		temp = b ;
+		b = c ;
+		c = temp;
+	}
+	if(R2 == 1)
+	{
+		;
+	}
+	if(R3 == 0)
+	{
+		temp = c ;
+		c = d ;
+		d = temp;
+	}
+	if(R3 == 1)
+	{
+		;
+	}
+	if(R4 == 0)
+	{
+		temp = d ;
+		d = a ;
+		a = temp;
+	}
+	if(R4 == 1)
+	{
+		;
+	}
+}
